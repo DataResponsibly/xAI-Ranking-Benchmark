ffb91a65833b286bc3a0f44ddc784630df47321a
joaopfonseca <jpmrfonseca@gmail.com>
2023-10-27 21:44:30 +0100

    REF format using black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='xai_ranking',
+    name="xai_ranking",
     packages=find_packages(),
-    version='0.1.0',
-    description='Ranking Explainability Benchmark',
-    author='dataresponsibly',
-    license='MIT',
+    version="0.1.0",
+    description="Ranking Explainability Benchmark",
+    author="dataresponsibly",
+    license="MIT",
 )
