55630f8c34caa53d5b8e99e0557222624e980de5
akhynkokateryna <kateryna.akhynko@ucu.edu.ua>
2023-11-06 23:40:24 +0200

    initial commit on tennis data preprocessing (#2)

---
+++ xai_ranking/data/merge_by_year.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+folder_path = '../../data/interim'
+csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+# dictionary with year as key, list of datasets about that year as value
+dfs_by_year = {}
+for csv_file in csv_files:
+    year = csv_file.split('_')[-1][:4]
+    df = pd.read_csv(os.path.join(folder_path, csv_file))
+    dfs_by_year.setdefault(year, []).append(df)
+
+merged_dfs = {}
+for year, dataframes in dfs_by_year.items():
+    merged_df = dataframes[0]
+    if len(dataframes) > 1:
+        for i, df in enumerate(dataframes[1:]):
+            merged_df = pd.merge(merged_df, df, on='standing_player2', how='outer')
+
+    merged_dfs[year] = merged_df
+
+for year, df in merged_dfs.items():
+    df.to_csv(f'../../data/processed/merged_{year}.csv')
