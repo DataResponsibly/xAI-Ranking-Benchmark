a7077e2a5d28faf352d44096dda827ae3a7d8188
joaopfonseca <jpfonseca@novaims.unl.pt>
2024-06-04 14:51:00 +0100

    MNT rename submodule

---
+++ xai_ranking/benchmarks/_hre.py
@@ -0,0 +1,8 @@
+"""
+Local Explanations of Global Rankings: Insights for Competitive Rankings
+
+Hierarchical Ranking Explanation (HRE) framework.
+
+Anahideh, H., & Mohabbati-Kalejahi, N. (2022). Local explanations of global
+rankings: insights for competitive rankings. IEEE Access, 10, 30676-30693.
+"""
