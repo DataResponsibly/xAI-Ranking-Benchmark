d747293aa91c4180c13b8cf18e49512bcaaa2a48
akhynkokateryna <kateryna.akhynko@ucu.edu.ua>
2024-07-02 16:06:58 +0300

    added max sensitivity experiment for ATP dataset

--- xai_ranking/metrics/__init__.py
+++ xai_ranking/metrics/__init__.py
@@ -1,5 +1,7 @@
 from .kendall_tau import kendall_tau
+from .max_sensitivity import compute_sensitivity as max_sensitivity
 
 __all__ = [
-    "kendall_tau"
+    "kendall_tau",
+    "max_sensitivity"
 ]
